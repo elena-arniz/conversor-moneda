69aa1579af1f5c1f934ff73e8ecbfd797756f0d4
Elena Arniz <elena.arniz.dev@gmail.com>
2024-08-09 11:57:25 +0200

    Cambios visuales y hacer la app responsive

--- src/App.js
+++ src/App.js
@@ -1,75 +1,70 @@
 import './App.css';
-
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 function App() {
-
   const monedaEntRef = useRef();
-
-  const monedaSalRef = useRef();
-
   const tipoEntrada = useRef();
-
   const tipoSalida = useRef();
 
+  // Estado para guardar el resultado del cambio
+  const [resultadoCambio, setResultadoCambio] = useState("");
+
   const llamaApiCambio = async () => {
-
     try {
-
       const respuesta = await fetch(`https://v6.exchangerate-api.com/v6/6aea8d5bc4a0517728dbcb31/latest/${tipoEntrada.current.value}`);
-
       const datos = await respuesta.json();
-
       return datos.conversion_rates[tipoSalida.current.value];
-
     } catch (error) {
       console.error("Error al acceder a la API: ", error);
-
     }
-
   };
 
   const calcular = async () => {
-
     const resultadoCambioNuevo = await llamaApiCambio();
-
     const valorEntrada = parseFloat(monedaEntRef.current.value);
-
     const valorSalida = (valorEntrada * resultadoCambioNuevo).toFixed(2);
 
-    monedaSalRef.current.innerHTML = `El valor de ${valorEntrada} ${tipoEntrada.current.value} es de ${valorSalida} ${tipoSalida.current.value}`; 
-
+    if (isNaN(valorEntrada)) {
+      setResultadoCambio('Introduce un valor numérico');
+    } else {
+      setResultadoCambio(`El valor de ${valorEntrada} ${tipoEntrada.current.value} es de ${valorSalida} ${tipoSalida.current.value}`);
+    }
   }
 
-  return <div>
-    <h1>Conversor de monedas</h1>
 
-    <div className='centrarElementos fondoBlanco'>
-      <label for="tipoEntrada">Moneda de origen</label>
-      <select className='selector' id="tipoEntrada" ref={tipoEntrada}>
-        <option value="EUR">EUR</option>
-        <option value="USD">USD</option>
-        <option value="JPY">JPY</option>
-        <option value="MXN">MXN</option>
-      </select><br />
+  return (
+    <div>
+      <h1>Conversor de monedas</h1>
 
-      <input className='input' type='number' ref={monedaEntRef}></input><br />
+      <div className='centrarElementos fondoBlanco'>
+        <label htmlFor="tipoEntrada">Moneda de origen</label>
+        <select className='selector' id="tipoEntrada" ref={tipoEntrada}>
+          <option value="EUR">EUR</option>
+          <option value="USD">USD</option>
+          <option value="JPY">JPY</option>
+          <option value="MXN">MXN</option>
+        </select><br />
 
-      <button className='botonCambiar' onClick={calcular}>Cambiar</button><br />
+        <input className='input' type='number' ref={monedaEntRef}></input><br />
 
-      <label for="tipoSalida">Moneda de cambio</label>
-      <select className='selector' id="tipoSalida" ref={tipoSalida}>
-        <option value="EUR">EUR</option>
-        <option value="USD">USD</option>
-        <option value="JPY">JPY</option>
-        <option value="MXN">MXN</option>
-      </select>
+        <button className='botonCambiar' onClick={calcular}>Cambiar</button><br />
+
+        <label htmlFor="tipoSalida">Moneda de cambio</label>
+        <select className='selector' id="tipoSalida" ref={tipoSalida}>
+          <option value="EUR">EUR</option>
+          <option value="USD">USD</option>
+          <option value="JPY">JPY</option>
+          <option value="MXN">MXN</option>
+        </select>
+      </div>
+
+      {resultadoCambio && (
+        <div className='centrarElementos resultado'>
+          {resultadoCambio}
+        </div>
+      )}
     </div>
-
-    <div className='centrarElementos resultado' ref={monedaSalRef}></div>
-   
-  </div>
-
+  );
 }
 
 export default App;
